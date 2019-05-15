eeaa352c27b1cf25508fbbf217c3a48470a9dab9
Walee Ahmed <dev@walee.me>
2019-05-15 04:48:00 -0400

    added initial deps & setup for redux

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,26 @@
 const common = {
-  preset: 'ts-jest'
+  preset: "ts-jest"
 }
 
 module.exports = {
   projects: [
     {
       ...common,
-      runner: '@jest-runner/electron/main',
-      testEnvironment: 'node',
-      testMatch: ['<rootDir>/src/main/**/*.(spec|test|).ts']
+      runner: "@jest-runner/electron/main",
+      testEnvironment: "node",
+      testMatch: [
+        "<rootDir>/src/main/**/*.(spec|test|).ts",
+        "<rootDir>/src/common/**/*.(spec|test|).ts"
+      ]
     },
     {
       ...common,
-      runner: '@jest-runner/electron',
-      testEnvironment: '@jest-runner/electron/environment',
+      runner: "@jest-runner/electron",
+      testEnvironment: "@jest-runner/electron/environment",
       testMatch: [
-        '<rootDir>/src/renderer/**/*.(spec|test|).ts',
-        '<rootDir>/src/renderer/**/*.(spec|test|).tsx'
+        "<rootDir>/src/renderer/**/*.(spec|test|).ts",
+        "<rootDir>/src/renderer/**/*.(spec|test|).tsx"
       ]
     }
   ]
 }
-
-
