4d042bb5365908009431171d337fddc6867d979d
Walee Ahmed <dev@walee.me>
2019-05-15 01:22:08 -0400

    tweak jest settings

--- jest.config.js
+++ jest.config.js
@@ -8,13 +8,16 @@ module.exports = {
       ...common,
       runner: '@jest-runner/electron/main',
       testEnvironment: 'node',
-      testMatch: ['**/__tests__/**/*.(spec|test).ts']
+      testMatch: ['<rootDir>/src/main/**/*.(spec|test|).ts']
     },
     {
       ...common,
       runner: '@jest-runner/electron',
       testEnvironment: '@jest-runner/electron/environment',
-      testMatch: ['**/__tests__/**/*.(spec|test).tsx']
+      testMatch: [
+        '<rootDir>/src/renderer/**/*.(spec|test|).ts',
+        '<rootDir>/src/renderer/**/*.(spec|test|).tsx'
+      ]
     }
   ]
 }
