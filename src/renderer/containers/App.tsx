ebdd7938ea03e25049f4417d8b62caa464614603
Walee Ahmed <dev@walee.me>
2019-05-14 23:14:14 -0400

    added react helmet and clean

--- src/renderer/containers/App.tsx
+++ src/renderer/containers/App.tsx
@@ -2,12 +2,13 @@ import React from "react"
 import { HashRouter } from "react-router-dom"
 import Container from "@material-ui/core/Container"
 import Box from "@material-ui/core/Box"
-
+import { Helmet } from "react-helmet"
 import Pages from "../pages"
 
 const App: React.FunctionComponent = (): React.ReactElement => (
   <Container>
     <Box>
+      <Helmet defaultTitle="Odioboek" titleTemplate="%s" />
       <HashRouter>
         <Pages />
       </HashRouter>
