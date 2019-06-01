961a957fe14e93f1173d03b4fa93c2b3ff20fd60
Walee Ahmed <dev@walee.me>
2019-06-01 06:00:57 -0400

    refactored drawer out of layout component

---
+++ src/renderer/componenets/NavDrawer.tsx
@@ -0,0 +1,70 @@
+import React, { FC, ReactElement } from "react"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import ListItemText from "@material-ui/core/ListItemText"
+import InboxIcon from "@material-ui/icons/MoveToInbox"
+import MailIcon from "@material-ui/icons/Mail"
+import List from "@material-ui/core/List"
+import Divider from "@material-ui/core/Divider"
+import Drawer from "@material-ui/core/Drawer"
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles"
+import { CSSProperties } from "@material-ui/styles"
+import cn from "classnames"
+
+const drawerWidth = 240
+
+const useStyles = makeStyles(
+  (
+    theme: Theme
+  ): Record<"drawer" | "drawerPaper" | "toolbar" | "drag", CSSProperties> =>
+    createStyles({
+      drawer: {
+        width: drawerWidth,
+        flexShrink: 0
+      },
+      drawerPaper: {
+        width: drawerWidth
+      },
+      toolbar: theme.mixins.toolbar,
+      drag: {
+        userSelect: "none",
+        WebkitAppRegion: "drag"
+      }
+    })
+)
+
+const NavDrawer = (): ReactElement => {
+  const classes = useStyles()
+
+  return (
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper
+      }}
+    >
+      <div className={cn(classes.toolbar, classes.drag)} />
+      <Divider />
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Hello"} />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemIcon>
+            <MailIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Hello"} />
+        </ListItem>
+        <Divider />
+      </List>
+    </Drawer>
+  )
+}
+
+export default NavDrawer
--- src/renderer/containers/Layout.tsx
+++ src/renderer/containers/Layout.tsx
@@ -1,50 +1,22 @@
 import React, { FC, ReactElement } from "react"
 import Container from "@material-ui/core/Container"
 import Box from "@material-ui/core/Box"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemIcon from "@material-ui/core/ListItemIcon"
-import ListItemText from "@material-ui/core/ListItemText"
-import InboxIcon from "@material-ui/icons/MoveToInbox"
-import MailIcon from "@material-ui/icons/Mail"
-import List from "@material-ui/core/List"
-import Divider from "@material-ui/core/Divider"
-import Drawer from "@material-ui/core/Drawer"
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles"
 import { CSSProperties } from "@material-ui/styles"
-import cn from "classnames"
 
 import TopBar from "../componenets/TopBar"
+import NavDrawer from "../componenets/NavDrawer"
 
 const drawerWidth = 240
 
 const useStyles = makeStyles(
-  (
-    theme: Theme
-  ): Record<
-    "root" | "drawer" | "drawerPaper" | "toolbar" | "content" | "drag",
-    CSSProperties
-  > =>
+  (theme: Theme): Record<"content", CSSProperties> =>
     createStyles({
-      root: {
-        display: "flex"
-      },
-      drawer: {
-        width: drawerWidth,
-        flexShrink: 0
-      },
-      drawerPaper: {
-        width: drawerWidth
-      },
-      toolbar: theme.mixins.toolbar,
       content: {
         flexGrow: 1,
         backgroundColor: theme.palette.background.default,
         padding: theme.spacing(3),
         marginLeft: drawerWidth
-      },
-      drag: {
-        userSelect: "none",
-        WebkitAppRegion: "drag"
       }
     })
 )
@@ -63,40 +35,11 @@ const Layout: FC<Props> = ({ title, children }): ReactElement => {
     </Container>
   )
 
-  const drawer = (
-    <Drawer
-      variant="permanent"
-      anchor="left"
-      className={classes.drawer}
-      classes={{
-        paper: classes.drawerPaper
-      }}
-    >
-      <div className={cn(classes.toolbar, classes.drag)} />
-      <Divider />
-      <List>
-        <ListItem button>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary={"Hello"} />
-        </ListItem>
-        <Divider />
-        <ListItem button>
-          <ListItemIcon>
-            <MailIcon />
-          </ListItemIcon>
-          <ListItemText primary={"Hello"} />
-        </ListItem>
-        <Divider />
-      </List>
-    </Drawer>
-  )
   return (
     <>
       <header>
         <TopBar title={title} />
-        {drawer}
+        <NavDrawer />
       </header>
       <main className={classes.content}>{main}</main>
     </>
