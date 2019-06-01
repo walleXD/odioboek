fa4bb172ae8490c8b86fecc24fcec70a96ff9871
Walee Ahmed <dev@walee.me>
2019-06-01 05:42:51 -0400

    updated deps and fix react types resolution
    https://stackoverflow.com/questions/52399839/typescript-duplicate-identifier-librarymanagedattributes

--- src/renderer/containers/__tests__/App.test.tsx
+++ src/renderer/containers/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import "jest-dom/extend-expect"
 import * as React from "react"
-import { render, cleanup } from "react-testing-library"
+import { render, cleanup } from "@testing-library/react"
 
 import App from "../App"
 
